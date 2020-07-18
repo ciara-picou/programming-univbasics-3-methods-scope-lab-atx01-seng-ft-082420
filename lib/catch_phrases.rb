f546f0bc1af287c95490cbde7cc60215571c2762
Ciara Picou <ciarapicou@att.net>
2020-07-18 21:07:10 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -10,4 +10,12 @@ def toadstool
 end
 
 def link
-  phrase = 
+  phrase =  'It's Dangerous To Go Alone! Take This.'
+  puts phrase
+end
+
+def all_phrases
+  puts mario
+  puts toadstool
+  pus link
+end
